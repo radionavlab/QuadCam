5a77146ce45e966a9d37664a5bf12c12f66ba1e2
tuckerhaydon <tuckerhaydon@gmail.com>
2018-06-14 04:47:22 -0500

    added timing mechanism to example client

--- examples/client.cc
+++ examples/client.cc
@@ -6,11 +6,35 @@
 #include <iostream>
 #include <unistd.h>
 #include <signal.h>
+#include <chrono>
 
 bool OK{true};
 
 void SigHandler(int s) {
     OK = false;
+}
+
+long long CurrentTimeMillis() {
+    return std::chrono::duration_cast< std::chrono::milliseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+}
+
+void TicToc() {
+    static long long previous = 0L;
+    static bool tic = false;
+
+    long long now = std::chrono::duration_cast< std::chrono::milliseconds >(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+
+    if(tic) {
+       tic = false;
+       std::cout << "Time Elapsed: " << now - previous << " ms" << std::endl; 
+    } else {
+        tic = true;
+        previous = now;
+    }
 }
 
 void ConfigureSigHandler() {
@@ -29,7 +53,9 @@ int main(int argc, char** argv) {
     quadcam::CameraClient client("/tmp/camera_server");
 
     while(OK) {
+        TicToc();
         quadcam::FrameData frame_data = client.RequestFrame();
+        TicToc();
         std::cout << frame_data.meta_data.width << ", " << frame_data.meta_data.height << std::endl;
     }
 
