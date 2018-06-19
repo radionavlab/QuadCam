25462dc4d991cc9787e2774cb28d0c96483333a9
tuckerhaydon <tuckerhaydon@gmail.com>
2018-06-19 04:15:55 -0500

    Removed extra spaces in frame_data

--- src/client/frame_data.h
+++ src/client/frame_data.h
@@ -9,39 +9,39 @@
 
 namespace quadcam {
 
-    typedef struct {
-        uint32_t data_size;
-        uint32_t width;
-        uint32_t height;
+typedef struct {
+    uint32_t data_size;
+    uint32_t width;
+    uint32_t height;
 
-        std::string Serialize() {
-            return ""
-                + std::to_string(data_size) + ","
-                + std::to_string(width) + ","
-                + std::to_string(height);
-        };
+    std::string Serialize() {
+        return ""
+            + std::to_string(data_size) + ","
+            + std::to_string(width) + ","
+            + std::to_string(height);
+    };
 
-        void Deserialize(const std::string& serial_data) {
-            std::stringstream ss(serial_data);
-            std::vector<uint32_t> frame_info_vec;
-            int i;
-            while (ss >> i) {
-                frame_info_vec.push_back(i);
+    void Deserialize(const std::string& serial_data) {
+        std::stringstream ss(serial_data);
+        std::vector<uint32_t> frame_info_vec;
+        int i;
+        while (ss >> i) {
+            frame_info_vec.push_back(i);
 
-                if (ss.peek() == ',')
-                    ss.ignore();
-            }
-            
-            data_size = frame_info_vec[0];
-            width     = frame_info_vec[1];
-            height    = frame_info_vec[2];
-        };
+            if (ss.peek() == ',')
+                ss.ignore();
+        }
+        
+        data_size = frame_info_vec[0];
+        width     = frame_info_vec[1];
+        height    = frame_info_vec[2];
+    };
 
-    } FrameMetaData;
+} FrameMetaData;
 
-    typedef struct {
-        FrameMetaData meta_data;
-        std::shared_ptr<uint8_t> data;
-    } FrameData;
+typedef struct {
+    FrameMetaData meta_data;
+    std::shared_ptr<uint8_t> data;
+} FrameData;
 
 }; // namespace quadcam
